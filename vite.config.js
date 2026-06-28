402617b9e6b8d3e76ba8897dd8b77ed503323ad6
MazenMohamedali / <mazenqwe347@gmail.com>
2026-06-28 06:19:24 +0300

    deployment config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/client-el-tellawi-store/",
+  base: "/el-tellawi-store/",
 });
